a95ecb8c03281bf230a4d1ce8caba50ebfd8e36d
SimonKvantdator <simjac@student.chalmers.se>
2019-06-28 10:09:55 +0200

    uppdaterade scale_and_match_trajectories

--- Find M/read_and_plot_trajectories.py
+++ Find M/read_and_plot_trajectories.py
@@ -4,7 +4,15 @@ import pandas
 from mpl_toolkits import mplot3d
 from collections import defaultdict
 import sys
-sys.path.insert(0, sys.path[0] + '/../smaple QTM OR ZED data')
+#sys.path.insert(0, sys.path[0] + '/../smaple QTM OR ZED data')
+
+def translate(x,y,z): #translates a list of (x,y,z) coordinates so that the first one is the origin
+	x_translated = np.array([u - x[0] for u in x])
+	y_translated = np.array([v - y[0] for v in y])
+	z_translated = np.array([w - z[0] for w in z])
+
+	return x_translated,y_translated,z_translated
+
 
 def get_qtm_data():
 
@@ -27,15 +35,10 @@ def get_qtm_data():
 	    for j in range(0, 2):
 	        qtm_rot[i,j] = qtm_data_matrix[:,6+i+j] # 5-16 contains rotation elements
 
+	x, y, z = translate(x,y,z) #ONLY FOR VISUAL COMPARISON BEFORE CALIBRATION
+	return x, y, z
 
-	### translate everything to the origin ###
-	x_translated = np.array([u - x[0] for u in x])
-	y_translated = np.array([v - y[0] for v in y])
-	z_translated = np.array([w - z[0] for w in z])
-
-	return x_translated,y_translated,z_translated
-
-
+	
 def get_zed_data():
 
 	file = 'zed_pose_data.txt'
@@ -51,12 +54,8 @@ def get_zed_data():
 	qz = df_np[:,5]
 	qw = df_np[:,6]
 
-	### translate everything to the origin ###
-	x_translated = np.array([u - x[0] for u in x])
-	y_translated = np.array([v - y[0] for v in y])
-	z_translated = np.array([w - z[0] for w in z])
-
-	return z_translated,-x_translated,y_translated #according to QTMS:s coordinate system
+	x, y, z = translate(x,y,z) #ONLY FOR VISUAL COMPARISON BEFORE CALIBRATION
+	return x, y, z
 
 
 def get_rift_data():
@@ -74,24 +73,14 @@ def get_rift_data():
 	qz = df_np[:,5]
 	qw = df_np[:,6]
 
-	i_array=[]
-	for i,_ in enumerate(x): #removes all (0,0,0):s
-		if (x[i]==0.0) * (y[i]==0.0) * (z[i]==0.0):
-			i_array.append(i)
+	
+	### Remove all the (0.0, 0.0, 0.0):s ###
+	index_array = [] #initiate
+	for index,_ in enumerate(x):
+		if (x[index]==0.0) * (y[index]==0.0) * (z[index]==0.0):
+			index_array.append(index)
 
-	x = np.delete(x,i_array)
-	y = np.delete(y,i_array)
-	z = np.delete(z,i_array)
-
-
-	### translate everything to the origin ###
-	x_translated = np.array([u - x[0] for u in x])
-	y_translated = np.array([v - y[0] for v in y])
-	z_translated = np.array([w - z[0] for w in z])
-
-	return z_translated,-x_translated,y_translated #according to QTMS:s coordinate system
-
-
+<<<<<<< Updated upstream
 qtm_traj = get_qtm_data()
 or_traj = get_rift_data()
 zed_traj = get_zed_data()
@@ -123,6 +112,45 @@ plt.ylabel('y', fontsize=24)
 #plt.zlabel('z')
 plt.legend()
 plt.show()
+=======
+	x = np.delete(x,index_array)
+	y = np.delete(y,index_array)
+	z = np.delete(z,index_array)
+
+	x, y, z = translate(x,y,z) #ONLY FOR VISUAL COMPARISON BEFORE CALIBRATION
+	return x, y, z
+
+
+def plot_trajectories():
+
+	qtm_traj = get_qtm_data()
+	or_traj = get_rift_data()
+	zed_traj = get_zed_data()
+
+	### 3d-plot ###
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+
+	x = qtm_traj[0]
+	y = qtm_traj[1]
+	z = qtm_traj[2]
+	max_range = np.array([x.max()-x.min(), y.max()-y.min(), z.max()-z.min()]).max() / 2.0
+	mid_x = (x.max()+x.min()) * 0.5
+	mid_y = (y.max()+y.min()) * 0.5
+	mid_z = (z.max()+z.min()) * 0.5
+	ax.set_xlim(mid_x - max_range, mid_x + max_range)
+	ax.set_ylim(mid_y - max_range, mid_y + max_range)
+	ax.set_zlim(mid_z - max_range, mid_z + max_range)
+	plt.title('QTM, OR, & ZED trajectories')
+
+	ax.plot3D(qtm_traj[0],qtm_traj[1],qtm_traj[2], color='r', label='QTM')
+	ax.plot3D(or_traj[0], or_traj[1], or_traj[2], color='b', label='OR')
+	#ax.plot3D(zed_traj[0], zed_traj[1], zed_traj[2], color='g', label='ZED')
+	plt.xlabel('x', fontsize=24)
+	plt.ylabel('y', fontsize=24)
+	plt.legend()
+	plt.show()
+>>>>>>> Stashed changes
 
 
 
--- Find M/scale_and_match_trajectories.py
+++ Find M/scale_and_match_trajectories.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 import pandas
 from mpl_toolkits import mplot3d
 from collections import defaultdict
@@ -9,5 +10,72 @@ sys.path.insert(0, sys.path[0] + '/../smaple QTM OR ZED data')
 from read_and_plot_trajectories import get_qtm_data
 from read_and_plot_trajectories import get_rift_data
 
-asd = get_qtm_data
-asdf = get_rift_data
+### BODGE ###
+I_qtm_first = 1862
+I_qtm_last = 2780
+I_or_first = 3193
+I_or_last = 7628
+
+
+qtm_traj = get_qtm_data()
+or_traj = get_rift_data()
+
+def handpalaggning(qtm_traj_var, or_traj_var): #BODGE
+	plt.figure()
+	plt.plot(qtm_traj_var[0] - qtm_traj_var[0][0], 'b', label='x(t) QTM')
+	plt.plot(qtm_traj_var[1] - qtm_traj_var[1][0], 'g', label='y(t) QTM')
+	plt.plot(qtm_traj_var[2] - qtm_traj_var[2][2], 'r', label='z(t) QTM')
+	plt.plot(or_traj_var[0] - or_traj_var[0][0], 'b:', label='x(t) OR')
+	plt.plot(or_traj_var[1] - or_traj_var[1][0], 'g:', label='y(t) OR')
+	plt.plot(or_traj_var[2] - or_traj_var[2][0], 'r:', label='z(t) OR')
+	plt.legend(fontsize=22)
+	plt.show()
+
+
+handpalaggning(qtm_traj, or_traj)
+
+
+tmp_qtm_array = [[], [], []]
+tmp_or_array = [[], [], []]
+for i in range(0,3): #BODGE
+
+	tmp = qtm_traj[i]
+	tmp_qtm_array[i] = tmp[I_qtm_first:I_qtm_last]
+	tmp = or_traj[i]
+	tmp_or_array[i] = tmp[I_or_first:I_or_last]
+
+qtm_traj = tmp_qtm_array
+or_traj = tmp_or_array
+
+
+qtm_traj_matched = [[], [] ,[]] #initiate
+or_traj_matched = [[], [] ,[]] #initiate
+t_index_qtm = np.arange(I_qtm_first,I_qtm_last)
+t_index_or = np.arange(I_or_first,I_or_last)
+t_common = np.linspace(0.01,0.99, num=int(1e4)) #num is the 
+for i in range(0,3):
+	f_qtm = interp1d(t_index_qtm, qtm_traj[i], kind='cubic')
+	f_or = interp1d(t_index_or, or_traj[i], kind='cubic')
+
+	qtm_traj_matched[i] = f_qtm(t_common*(I_qtm_last-I_qtm_first)+I_qtm_first)
+	or_traj_matched[i] = f_or(t_common*(I_or_last-I_or_first)+I_or_first)
+
+
+handpalaggning(qtm_traj_matched, or_traj_matched)
+
+#def object_fun(M): #pseudocode
+#	sum = 0 #initiate
+#	sum += np.trapz(abs((r_OR - M r_QTM)^2))
+#	return sum
+
+
+
+
+
+
+
+
+
+
+
+
