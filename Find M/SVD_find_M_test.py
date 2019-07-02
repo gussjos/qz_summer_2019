0db596f01dfdf2eefc2e1cb52ae7448010185897
SimonKvantdator <47160031+SimonKvantdator@users.noreply.github.com>
2019-07-02 08:52:39 +0200

    upd8

--- Find M/SVD_find_M_test.py
+++ Find M/SVD_find_M_test.py
@@ -14,6 +14,12 @@ from read_and_plot_trajectories import get_rift_data
 from read_and_plot_trajectories import plot_trajectories #BODGE
 from scale_and_match_trajectories import match_trajectories
 qtm_traj_matched, or_traj_matched = match_trajectories(get_qtm_data(), get_rift_data())
+#tmp0 = qtm_traj_matched[:,0]
+#tmp1 = qtm_traj_matched[:,1]
+#tmp2 = qtm_traj_matched[:,2]
+#qtm_traj_matched[:,0] = tmp1
+#qtm_traj_matched[:,1] = -tmp0
+#qtm_traj_matched[:,2] = qtm_traj_matched[:,2]
 
 #plt.plot(qtm_traj_matched[:,0], 'r')
 #plt.plot(qtm_traj_matched[:,1], 'g')
@@ -45,12 +51,10 @@ def get_rotation_and_translation(qdata, pdata): # finds R & t in eq  p=Rq+t wher
 
 	U, S, V = np.linalg.svd(H)
 
-	R = V.dot(U.transpose())
-	if (np.linalg.det(R) < 0): #slight bodge, it might happen that R is a reflection, then redefine R by doing the following
-		print('det R = ' + str(np.linalg.det(R)))
-		V[:,-1] = -V[:,-1]
-		R = V.dot(U.transpose())
+	d = np.linalg.det(V.dot(U.T))
+	D = np.array([[d, 0, 0], [0, d, 0], [0, 0, 1]])
 
+	R = V.dot(D).dot(U.T)
 	t = centroid_p - R.dot(centroid_q) #p=Rq+t
 
 	return R,t
@@ -80,9 +84,9 @@ def plot_transformed_trajectories(qdata, pdata): #p=Rq+t
 	ax.set_xlim(mid_x - max_range, mid_x + max_range)
 	ax.set_ylim(mid_y - max_range, mid_y + max_range)
 	ax.set_zlim(mid_z - max_range, mid_z + max_range)
-	plt.title('QTM, OR, & ZED trajectories')
+	plt.title('QTM & OR trajectories')
 
-	ax.plot3D(qdata_transformed[:,0],qdata_transformed[:,1],qdata_transformed[:,2], color='r', label='QTM')
+	ax.plot3D(qdata_transformed[:,0], qdata_transformed[:,1], qdata_transformed[:,2], color='r', label='QTM')
 	ax.plot3D(or_traj_matched[:,0], or_traj_matched[:,1], or_traj_matched[:,2], color='b', label='OR')
 	#ax.plot3D(zed_traj[0], zed_traj[1], zed_traj[2], color='g', label='ZED')
 	plt.xlabel('x', fontsize=24)
